df2644ae2a59c81fb106df9cdacdffee4d030dda
Parth-Gajera <126321337+Parth-Gajera@users.noreply.github.com>
2023-05-26 12:20:34 +0530

    Update Portfolio.js

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -7,9 +7,9 @@ import star from "../img/star .png";
 
 
 const images = [
-  { src: "https://demo.epic-webdesign.com/tf-besmart/v1/images/p1.jpg", alt: 'Image 1'},
-  { src: "https://demo.epic-webdesign.com/tf-besmart/v1/images/p2.jpg", alt: 'Image 2'},
-  { src: 'https://demo.epic-webdesign.com/tf-besmart/v1/images/p3.jpg', alt: 'Image 3'},
+  { src: "https://demo.epic-webdesign.com/tf-besmart/v1/images/p1.jpg", alt: 'Image 1' },
+  { src: "https://demo.epic-webdesign.com/tf-besmart/v1/images/p2.jpg", alt: 'Image 2' },
+  { src: 'https://demo.epic-webdesign.com/tf-besmart/v1/images/p3.jpg', alt: 'Image 3' },
   { src: 'https://demo.epic-webdesign.com/tf-besmart/v1/images/p4.jpg', alt: 'Image 4' },
   { src: 'https://demo.epic-webdesign.com/tf-besmart/v1/images/p5.jpg', alt: 'Image 5' },
   { src: 'https://demo.epic-webdesign.com/tf-besmart/v1/images/p6.jpg', alt: 'Image 6' },
@@ -21,18 +21,18 @@ const images = [
 const Portfolio = () => {
   return (
     <section className="portfolio" id="Portfolio">
-      
+
       <div className="container">
         <h2 className='head2'>Our Latest Work</h2>
         <div className="deco">
           <span className="star"><img src={star} alt="" width="100%" /></span>
         </div>
-        <p className='text-center' style={{ opacity: '.5',marginBottom:'40px' }}>There are many variations of passages of Lorem Ipsum available, but the majority
+        <p className='text-center' style={{ opacity: '.5', marginBottom: '40px' }}>There are many variations of passages of Lorem Ipsum available, but the majority
           have suffered alteration, by injected humour, or new randomised words.
         </p>
 
         <Lightbox images={images} />
-       
+
 
       </div>
     </section>
