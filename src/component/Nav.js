caf6f11280d41072a9b355f6522947c286f47541
Parth-Gajera <126321337+Parth-Gajera@users.noreply.github.com>
2023-05-26 12:20:05 +0530

    Update Nav.js

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,39 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Component } from "react";
 
-// import { Link } from 'react-router-dom'
+
 import { HashLink } from 'react-router-hash-link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-const Nav = () => {
-   
-  
 
-        const [activeSection, setActiveSection] = useState(null);
-      
-        useEffect(() => {
-          const handleScroll = () => {
+const Nav = () => {
+
+
+
+    const [activeSection, setActiveSection] = useState(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
             const sections = document.querySelectorAll("section");
             const scrollPosition = window.pageYOffset;
-      
+
             sections.forEach((section, index) => {
-              const top = section.offsetTop;
-              const height = section.offsetHeight;
-      
-              if (
-                scrollPosition >= top - window.innerHeight * 0.5 &&
-                scrollPosition < top + height - window.innerHeight * 0.5
-              ) {
-                setActiveSection(index);
-              }
+                const top = section.offsetTop;
+                const height = section.offsetHeight;
+
+                if (
+                    scrollPosition >= top - window.innerHeight * 0.5 &&
+                    scrollPosition < top + height - window.innerHeight * 0.5
+                ) {
+                    setActiveSection(index);
+                }
             });
-          };
-      
-          window.addEventListener("scroll", handleScroll);
-      
-          return () => window.removeEventListener("scroll", handleScroll);
-        }, []);
-    
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
 
     return (
         <div className="container-fluid" id="Nav">
@@ -55,7 +56,7 @@ const Nav = () => {
 
                         <li className={activeSection === 0 ? "active" : ""} >
                             <HashLink
-                                to="#Home" 
+                                to="#Home"
                             >
                                 Home
                             </HashLink>
@@ -85,7 +86,7 @@ const Nav = () => {
                             </HashLink>
 
                         </li>
-                      
+
                         <li className={activeSection === 4 ? "active" : ""}>
                             <HashLink
                                 to="#Team"
@@ -94,7 +95,7 @@ const Nav = () => {
                             </HashLink>
 
                         </li>
-                      
+
                         <li className={activeSection === 5 ? "active" : ""}>
                             <HashLink
                                 to="#Services"
@@ -103,7 +104,7 @@ const Nav = () => {
                             </HashLink>
 
                         </li>
-                        
+
                         <li className={activeSection === 6 ? "active" : ""}>
                             <HashLink
                                 to="#Blog"
@@ -112,7 +113,7 @@ const Nav = () => {
                             </HashLink>
 
                         </li>
-                       
+
                         <li className={activeSection === 7 ? "active" : ""}>
                             <HashLink
                                 to="#Pricing"
@@ -120,14 +121,17 @@ const Nav = () => {
                                 Pricing
                             </HashLink>
                         </li>
-        
-                    </ul>
 
-                            <HashLink
-                                to="#Contact" className="contact-btn"
-                            >Contact us
-                            </HashLink>
-                    
+
+                    </ul>
+                    <div className="contact-tab">
+                        <HashLink
+                            to="#Contact" className="contact-btn"
+                        >Contact us
+                        </HashLink>
+                    </div>
+
+
                 </div>
 
             </nav >
